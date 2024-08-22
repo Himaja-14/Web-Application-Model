cecbf288f9ea585c38aa1de8d2a1a527ed3d5a58
vijay sathish <vijay.sathish@infor.com>
2024-08-22 23:26:21 +0530

    admin candidate seperation

--- hrm/settings.py
+++ hrm/settings.py
@@ -142,3 +142,15 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+#SMTP Configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'himajasammeta18@gmail.com'
+EMAIL_HOST_PASSWORD = 'fbedmaekcujqewoi'
+APPLICATION_EMAIL = 'Admin<himajasammeta18@gmail.com>'
+DEFAULT_FROM_EMAIL = 'Admin<himajasammeta18@gmail.com>'
+
